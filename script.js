785d474212de0763901afc930d7f9f3717988918
DePram <dprameshwara@gmail.com>
2024-07-28 14:29:28 +0800

    bikin rsvp

--- script.js
+++ script.js
@@ -25,3 +25,44 @@ hamburger.addEventListener("click", function () {
 close.addEventListener("hidden.bs.offcanvas", function () {
   stickyTop.style.overflow = "hidden";
 });
+
+// fitur scroll disable
+const rootElement = document.querySelector(":root");
+function disableScroll() {
+  scrollTop = window.scrollY || document.documentElement.scrollTop;
+  scrollLeft = window.scrollX || document.documentElement.scrollLeft;
+
+  window.onscroll = function () {
+    window.scrollTo(scrollTop, scrollLeft);
+  };
+
+  rootElement.style.scrollBehavior = "auto";
+}
+
+function enableScroll() {
+  window.onscroll = function () {};
+  rootElement.style.scrollBehavior = "smooth";
+  localStorage.setItem("opened", "true");
+}
+
+if (!localStorage.getItem("opened")) {
+  disableScroll();
+}
+
+// disableScroll();
+
+// form
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Konfirmasi Berhasil terkirim!");
+    });
+  });
+});
